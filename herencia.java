f5347b1cf72e4ecb72e4dbe125683d9deaec557f
Mariano A <mariano.salvador@anahuac.mx>
2026-08-17 11:34:47 -0600

    Add Vehiculo and Coche classes

---
+++ herencia.java
@@ -0,0 +1,11 @@
+class Vehiculo {
+    void acelerar() {
+        System.out.println("El vehículo está acelerando");
+    }
+}
+
+class Coche extends Vehiculo {
+    void abrirMaletero() {
+        System.out.println("El maletero del coche está abierto");
+    }
+}
