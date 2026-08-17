4ef4799579a48142ede855993514dd2d6f22160f
santiagomolinamercado1 <santiagomolinamercado1@gmail.com>
2026-08-16 19:13:08 -0500

    Update calculadora.java
    solo puse la funcion de suma, y explique porque salen los numeros con muchos decimales en la suma y restas de decimales

--- calculadora.java
+++ calculadora.java
@@ -118,7 +118,7 @@ public class calculadora extends JFrame implements ActionListener {
              pantalla.setText(String.valueOf(resultado));
             nuevaEntrada = true;
         } 
-//#endregion la cosa de los puntos decimales es por lo del binario, como no se puede calcular exacto los numeros los pone como aproximaciones, la solucion es igular la variable de resultado, y luego multiplciarla por 100000000 y lo dividirla por ese mismo numero, despues usas la funcion de math.round. si haces eso antes de la linea de codigo 118, se recorren 8 decimales y no te da un numero todo horrible      
+//#endregion la cosa de los puntos decimales es por lo del binario, como no se puede calcular exacto los numeros los pone como aproximaciones, la solucion es igular la variable de resultado, y luego multiplciarla por 100000000 y lo dividirla por ese mismo numero, despues usas la funcion de math.round. si haces eso antes de la linea de codigo 118, se recorren 8 decimales y no te da un numero todo horrible.      
 
                     
 
