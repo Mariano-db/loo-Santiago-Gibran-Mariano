9a90ea9f190590779ab644e6798896ac1a90a75d
Mariano A <mariano.salvador@anahuac.mx>
2026-08-17 11:35:29 -0600

    Create polimorfismo.java

---
+++ polimorfismo.java
@@ -0,0 +1,17 @@
+class Figura {
+    void dibujar() {
+        System.out.println("Dibujando figura");
+    }
+}
+
+class Circulo extends Figura {
+    void dibujar() {
+        System.out.println("Dibujando círculo");
+    }
+}
+
+class Cuadrado extends Figura {
+    void dibujar() {
+        System.out.println("Dibujando cuadrado");
+    }
+}
